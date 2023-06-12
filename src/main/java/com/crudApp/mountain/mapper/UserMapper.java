ad4a39971e2c574161590a44225dafd461af6e7a
bednarekdomi@gmail.com <bednarekdomi@gmail.com>
2023-06-12 22:14:46 +0200

    refactor of userMapper class

--- src/main/java/com/crudApp/mountain/mapper/UserMapper.java
+++ src/main/java/com/crudApp/mountain/mapper/UserMapper.java
@@ -8,20 +8,20 @@ import java.util.List;
 @Mapper(uses = {MountainMapper.class}, componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "userName", target = "userName")
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
-    @Mapping(source = "email", target = "email")
-    @Mapping(ignore = true, target = "userRatings")
-    @Mapping(source = "mountains", target = "mountains")
-    @Mapping(source = "roles", target = "roles")
-    UserEntityDto mapToUserDto(UserEntity userEntity);
+        @Mapping(source = "id", target = "id")
+        @Mapping(source = "userName", target = "userName")
+        @Mapping(source = "firstName", target = "firstName")
+        @Mapping(source = "lastName", target = "lastName")
+        @Mapping(source = "email", target = "email")
+        @Mapping(source = "userRatings", target = "userRatings")
+        @Mapping(source = "mountains", target = "mountains")
+        @Mapping(source = "roles", target = "roles")
+        UserEntityDto.UserEntityDtoBuilder mapToUserDto(UserEntity userEntity);
 
-    @InheritInverseConfiguration
-    @Mapping(ignore = true, target = "userRatings")
-    UserEntity mapToUser(UserEntityDto userEntityDto);
+        @InheritInverseConfiguration(name = "mapToUserDto")
+        UserEntity.UserEntityBuilder mapToUser(UserEntityDto userEntityDto);
 
-    @IterableMapping(elementTargetType = UserEntityDto.class)
-    List<UserEntityDto> mapToUserDtoList(List<UserEntity> userEntityList);
+        @IterableMapping(elementTargetType = UserEntityDto.class)
+        List<UserEntityDto> mapToUserDtoList(List<UserEntity> userEntityList);
+    }
 }
