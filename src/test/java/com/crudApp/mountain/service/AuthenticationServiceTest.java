dd7253d7728863767aa207e5f5b070d9b6fecd2d
bednarekdomi@gmail.com <bednarekdomi@gmail.com>
2023-11-27 12:06:27 +0100

    added new test case - shouldChangePassword()

--- src/test/java/com/crudApp/mountain/service/AuthenticationServiceTest.java
+++ src/test/java/com/crudApp/mountain/service/AuthenticationServiceTest.java
@@ -1,8 +1,8 @@
 package com.crudApp.mountain.service;
 
-import com.crudApp.mountain.domain.AuthenticationResponseDto;
 import com.crudApp.mountain.domain.LoginDto;
 import com.crudApp.mountain.domain.RegisterDto;
+import com.crudApp.mountain.domain.UserEntity;
 import com.crudApp.mountain.repository.RoleRepository;
 import com.crudApp.mountain.repository.UserRepository;
 import com.crudApp.mountain.security.JwtGenerator;
@@ -19,8 +19,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -44,12 +45,15 @@ public class AuthenticationServiceTest {
     private RegisterDto userOne;
     private RegisterDto userTwo;
     private LoginDto userOneLoginDetails;
+    private UserEntity userEntityOne;
 
     @Before
     public void setUp() {
         userOne = new RegisterDto("UserOne", "Password", "John", "Smith", "jsmith@email.com");
         userTwo = new RegisterDto("UserOne", "password", "Taylor", "Jones", "taylor@email.com");
         userOneLoginDetails = new LoginDto("UserOne", "Password".toCharArray());
+        userEntityOne = new UserEntity.UserEntityBuilder().id(1L).userName("user97").firstName("Susan").lastName("Jones")
+                .email("susan97@gmail.com").build();
     }
 
     @Test
@@ -95,5 +99,17 @@ public class AuthenticationServiceTest {
         assertEquals("Authentication error - incorrect login or password", response.getBody());
         verify(authenticationManager, times(1)).authenticate(any());
         verifyNoMoreInteractions(authenticationManager, jwtGenerator);
+    }
+
+    @Test
+    public void shouldChangePassword(){
+        //Given
+        when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(userEntityOne));
+        //When
+        ResponseEntity<String> response = authenticationService.changePassword(1L, "newPassword");
+        //Then
+        assertEquals(response.getBody(), "Password changed successfully");
+        assertEquals(userEntityOne.getPassword(), "newPassword".toCharArray());
+        verify(userRepository, times(1)).save(any());
     }
 }
